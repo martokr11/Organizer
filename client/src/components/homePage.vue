ac79ef121041376a4734e5cd62f63bccdcd837bf
martokr11 <martokr11@gmail.com>
2023-07-13 17:40:00 +0300

    updated

--- client/src/components/homePage.vue
+++ client/src/components/homePage.vue
@@ -2,13 +2,19 @@
   <q-layout view="hHh LpR fFf">
     <topMenu/>
     <leftDrawer/>
-  </q-layout>
 
-  <q-page-container>
+    <q-page-container>
     <q-page>
-        
+        <div class="row">
+            <div class="bg-green-6 column col-6"></div>
+            <div class="column col-6">
+                <div class="bg-pink-6">2</div>
+                <div class="bg-blue-6">3</div>
+            </div>
+        </div>
     </q-page>
   </q-page-container>
+  </q-layout>
 </template>
 
 <script setup>
