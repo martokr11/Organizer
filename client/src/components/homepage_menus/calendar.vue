2136732622bf6d299c1bfb816b3c30a76123c734
martokr11 <martokr11@gmail.com>
2023-08-07 15:53:00 +0300

    aaa

--- client/src/components/homepage_menus/calendar.vue
+++ client/src/components/homepage_menus/calendar.vue
@@ -1,16 +1,31 @@
 <template>
-  <div class="q-pa-md">
+  <div class="q-pa-md row" style="justify-content: center;">
     <div class="q-gutter-md">
-      <q-date v-model="date" :events="events" />
+      <q-date 
+        v-model="date"
+        :events="events"
+        mask="MM/DD/YYYY" 
+        style="width: 500px;"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
   import { ref } from 'vue';
+  let today = new Date().toLocaleDateString();
 
-  const date = ref('2019/02/01');
+  const p1 = '0' + today.split('/')[0];
+  const p2 = '0' + today.split('/')[1];
+  const p3 =       today.split('/')[2];
+
+  today = `${p1}/${p2}/${p3}`;
+
+ 
+  const date = ref(today);
   const events = [ '2019/02/01', '2019/02/05', '2019/02/06', '2019/02/09', '2019/02/23' ];
+
+  console.log(date.value)
 
   function eventsFn (date) {
     const parts = date.split('/')
