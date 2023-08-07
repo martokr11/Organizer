7e1a89f3610a498aed86c8f57b06a2d2946dd772
martokr11 <martokr11@gmail.com>
2023-08-07 14:54:06 +0300

    ..

--- client/src/components/homepage_menus/calendar.vue
+++ client/src/components/homepage_menus/calendar.vue
@@ -1,70 +1,19 @@
 <template>
-  <div class="calendar">
-    <div class="calendarHeader">
-      <p class="calendarMonth">{{ currentMonth }}</p>
-      <div class="moveBtns">
-          <q-btn @click="prevMonth" class="moveMonth" icon="arrow_left" rounded></q-btn>
-          <q-btn @click="nextMonth" class="moveMonth" icon="arrow_right"></q-btn>
-      </div>
-      </div>    
-    <div class="days">
-      <div v-for="day in daysInMonth" :key="day" @click="test" class="day">{{ day }}</div>
+  <div class="q-pa-md">
+    <div class="q-gutter-md">
+      <q-date v-model="date" :events="events" />
     </div>
   </div>
 </template>
 
 <script setup>
-import '../../assets/calendar.css'
-import { ref, computed } from 'vue';
+  import { ref } from 'vue';
 
-// Get the current date
-const currentDate = new Date();
+  const date = ref('2019/02/01');
+  const events = [ '2019/02/01', '2019/02/05', '2019/02/06', '2019/02/09', '2019/02/23' ];
 
-// Reactive variables
-const currentMonthIndex = ref(currentDate.getMonth());
-const currentYear = ref(currentDate.getFullYear());
-
-// Compute the number of days in the current month
-const daysInMonth = computed(() => {
-  const lastDay = new Date(currentYear.value, currentMonthIndex.value + 1, 0).getDate();
-  return Array.from({ length: lastDay }, (_, index) => index + 1);
-});
-
-// Compute the current month name
-const currentMonth = computed(() => {
-  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  return monthNames[currentMonthIndex.value];
-});
-
-// Function to navigate to the previous month
-const prevMonth = () => {
-  if (currentMonthIndex.value === 0) {
-    currentYear.value--;
-    currentMonthIndex.value = 11;
-  } else {
-    currentMonthIndex.value--;
+  function eventsFn (date) {
+    const parts = date.split('/')
+    return parts[ 2 ] % 2 === 0
   }
-};
-
-// Function to navigate to the next month
-const nextMonth = () => {
-  if (currentMonthIndex.value === 11) {
-    currentYear.value++;
-    currentMonthIndex.value = 0;
-  } else {
-    currentMonthIndex.value++;
-  }
-};
-
-const test = (day) => {
-  console.log(day.target);
-}
 </script>
-  
-<style>
-
-button {
-  cursor: pointer;
-}
-</style>
-  
