8a52cdb33b52901e83b6528c300988c318606f8e
martokr11 <martokr11@gmail.com>
2023-08-07 16:46:31 +0300

    aa

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { patch } from '../server/router/router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: "127.0.0.1",
+    proxy: {
+      '/~': {
+        target: 'https://127.0.0.1:1313',
+        rewrite: path => path.replace(/^\/~/, ''),
+      }
     }
   }
 })
